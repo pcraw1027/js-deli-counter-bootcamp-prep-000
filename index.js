4eeffaf7304985ce4965b7a17358ca5d82b20438
Peter Crawford <pcraw1027@gmail.com>
2021-02-08 18:00:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function currentLine(currentLinePeople) {
     for (i = 0; i < currentLinePeople.length; i++) {
       num = i + 1;
       theLineString = theLineString + num + ". " + currentLinePeople[i];
-      while (num < currentLinePeople.length) {
+      while (i < currentLinePeople.length) {
         theLineString = theLineString + " ,";
       }
     }
